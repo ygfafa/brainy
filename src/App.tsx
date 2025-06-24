64882ebf4affdf9d830869095622ed4235e94dfe
박종혁 <jonghyeok@jobis.co>
2025-06-25 07:02:30 +0900

    feat: sooner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
-
+import { Toaster, toast } from "sonner";
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <Button onClick={() => toast.success("Hello")}>Click me</Button>
+      <Toaster position="bottom-center" richColors expand />
     </div>
   );
 }
