99818dd557db8f7fa46649ba46639f34d8b5893c
박종혁 <jonghyeok@jobis.co>
2025-06-26 14:16:13 +0900

    feat: 라우터 구조

---
+++ src/app/provider.tsx
@@ -0,0 +1,42 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import * as React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { HelmetProvider } from "react-helmet-async";
+
+import { Spinner } from "@/components/ui/spinner";
+import { queryConfig } from "@/lib/react-query";
+import { Toaster } from "sonner";
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: queryConfig,
+      })
+  );
+
+  return (
+    <React.Suspense
+      fallback={
+        <div className="flex h-screen w-screen items-center justify-center">
+          <Spinner size="xl" />
+        </div>
+      }
+    >
+      <ErrorBoundary FallbackComponent={() => <div>Root Error Fallback</div>}>
+        <HelmetProvider>
+          <QueryClientProvider client={queryClient}>
+            {import.meta.env.DEV && <ReactQueryDevtools />}
+            <Toaster position="bottom-center" richColors expand />
+            <div>{children}</div>
+          </QueryClientProvider>
+        </HelmetProvider>
+      </ErrorBoundary>
+    </React.Suspense>
+  );
+};
