2dc5c6b8aa5a7d3cec625f3476d9fd5fd01cb9c0
박종혁 <jonghyeok@jobis.co>
2025-10-02 13:15:04 +0900

    모달 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,56 +1,59 @@
-import { Slot } from '@radix-ui/react-slot'
+import { IconLoader } from '@tabler/icons-react'
 import { cva, type VariantProps } from 'class-variance-authority'
-import * as React from 'react'
+import React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  'inline-flex select-none items-center justify-center whitespace-nowrap rounded font-medium leading-none transition-colors disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+        primary: 'bg-primary text-white shadow',
+        secondary: 'bg-secondary text-secondary-foreground shadow-sm',
+        ghost: '',
+        outlined: 'border border-gray-300 bg-white text-gray-500',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded px-3 text-xs',
+        lg: 'h-12 rounded px-8',
+        icon: 'h-9 w-9',
+      },
+      danger: {
+        true: 'border-danger-foreground bg-danger text-white',
+      },
+      loading: {
+        true: 'pointer-events-none opacity-50',
+      },
+      block: {
+        true: 'w-full',
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default',
     },
   },
 )
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
-  const Comp = asChild ? Slot : 'button'
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>
 
-  return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
-  )
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, block, loading, danger, children, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, danger, className, block, loading }))}
+        ref={ref}
+        {...props}
+      >
+        {loading && <IconLoader className="mr-2 h-4 w-4 animate-spin" />}
+        {children}
+      </button>
+    )
+  },
+)
+Button.displayName = 'Button'
 
 export { Button, buttonVariants }
---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,78 @@
+import * as ModalPrimitive from '@radix-ui/react-alert-dialog'
+import React from 'react'
+
+import { MAX_MOBILE_SCREEN_WIDTH } from '@/config/app'
+import { cn } from '@/lib/utils'
+
+import { Button, type ButtonProps } from './button'
+
+type ModalProps = {
+  title?: string
+  description?: string | React.ReactNode
+  open?: boolean
+  onOk?(): void
+  onCancel?(): void
+  okText?: string
+  cancelText?: string
+  okButtonProps?: ButtonProps
+  cancelButtonProps?: ButtonProps
+  hideCancelButton?: boolean
+  classNames?: {
+    title?: string
+    description?: string
+  }
+}
+
+export const Modal = ({
+  title,
+  description,
+  open,
+  onOk,
+  onCancel,
+  classNames,
+  okButtonProps,
+  cancelButtonProps,
+  hideCancelButton = false,
+  okText = 'Confirm',
+  cancelText = 'Cancel',
+}: ModalProps) => {
+  return (
+    <ModalPrimitive.Root open={open}>
+      <ModalPrimitive.Portal>
+        <ModalPrimitive.Overlay className="fixed inset-0 z-50 bg-black/40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+        <ModalPrimitive.Content
+          style={{
+            width: 'calc(100% - 32px)',
+            maxWidth: MAX_MOBILE_SCREEN_WIDTH - 32,
+          }}
+          className="fixed left-[50%] top-[50%] z-50 w-full translate-x-[-50%] translate-y-[-50%] rounded bg-white p-5 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom"
+        >
+          {title && (
+            <ModalPrimitive.Title
+              className={cn('mb-2 text-center text-lg font-bold', classNames?.title)}
+            >
+              {title}
+            </ModalPrimitive.Title>
+          )}
+          {description && (
+            <ModalPrimitive.Description className={cn('mb-5 text-center', classNames?.description)}>
+              {description}
+            </ModalPrimitive.Description>
+          )}
+
+          <div className="flex gap-2">
+            {!hideCancelButton && (
+              <Button variant="outlined" block onClick={onCancel} {...cancelButtonProps}>
+                {cancelText}
+              </Button>
+            )}
+
+            <Button variant="primary" block onClick={onOk} {...okButtonProps}>
+              {okText}
+            </Button>
+          </div>
+        </ModalPrimitive.Content>
+      </ModalPrimitive.Portal>
+    </ModalPrimitive.Root>
+  )
+}
